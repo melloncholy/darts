a6b33d2a9583a077f9dcd92874666965527e644a
melloncholy <42149601+melloncholy@users.noreply.github.com>
2023-08-06 18:09:52 +0300

    first version

---
+++ src/main/java/com/example/darts/DartsApplication.java
@@ -0,0 +1,13 @@
+package com.example.darts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DartsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DartsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/darts/DartsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.darts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DartsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
